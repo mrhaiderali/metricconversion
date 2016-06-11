82d95d1f2f8d47dc6c92908a62f1dc94bb1fcfb1
rana haider <rana haider>
2016-06-11 12:22:08 +0500

    Length coversion tool

---
+++ media/my/js/jsfile.js
@@ -0,0 +1,10 @@
+function convert(){
+	var input_number=document.getElementById("input_num").value;
+    input_num =parseInt(input_num);
+   var from_val =document.getElementById("from_val").selected;
+   var to_val =document.getElementById("to_val").selected;
+  console.log(from_val);
+  console.log(to_val);
+ 
+ 
+	}
